0e14dc1b4ec5349d690807ec2233382e23a10680
Antonia Baies <baies.antonia@gmail.com>
2023-10-30 14:52:59 +0100

    maybeitem add

--- Project.hs
+++ Project.hs
@@ -1,4 +1,6 @@
-data Item = Nothing | Rock | Spider | Baby 
+data Item = Rock | Spider | Baby 
+    deriving (Show, Eq)
+data MaybeItem = Maybe Item
     deriving (Show, Eq)
 data Bin Item = Leaf Item | Node Item (Bin Item) (Bin Item)
     deriving (Show, Eq)
