76335d86ed68661f346782daf4689a0e07fb1397
Antonia Baies <baies.antonia@gmail.com>
2023-10-30 15:41:20 +0100

    add bin context

--- Project.hs
+++ Project.hs
@@ -2,3 +2,6 @@ data Item = Rock | Spider | Baby
     deriving (Show, Eq)
 data Bin (Maybe Item) = Leaf (Maybe Item) | Node (Maybe Item) (Bin (Maybe Item)) (Bin (Maybe Item))
     deriving (Show, Eq)
+
+data BinCxt = Hole | B0 BinCxt (Bin (Maybe Item)) | B1 (Bin (Maybe Item)) BinCxt
+    deriving (Show, Eq)
