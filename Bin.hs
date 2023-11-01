5e70aca624f20af40a75c9f68a3c9329cb7e1fbe
Antonia Baies <baies.antonia@gmail.com>
2023-11-01 23:38:02 +0100

    printing + tree gen

--- Bin.hs
+++ Bin.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
+import System.IO
+import System.Random
+
 module Bin where
 
-data Item = Rock | Spider | Baby
+data Item = Rock | Spider | Baby | NonExistant
     deriving (Show, Eq)
 
 data Bin a = Leaf (Maybe a) | Node (Maybe a) (Bin a) (Bin a)
@@ -17,7 +20,7 @@ data UpdatableFlag a = NoFlag | Flag (BinCxt a)
 type BinZip a = (BinCxt a, Bin a)
 
 
--- This will print all the items found on the tree 
+-- debugger function : prints all the items found in a tree
 printLabels :: Bin Item -> IO ()
 printLabels (Leaf Nothing) = putStrLn "Empty Leaf"
 printLabels (Leaf (Just item)) = putStrLn $ "Leaf: " ++ show item
@@ -30,26 +33,6 @@ printLabels (Node (Just item) left right) = do
     printLabels left
     printLabels right
 
---test_tree1 = Node (Just Rock) (Leaf (Just Spider)) (Leaf (Just Baby)) 
---test_tree2 = Node (Nothing) (Leaf (Just Rock)) (Node (Just Spider) (Leaf (Nothing)) (Leaf (Just Spider)))
-
-{-plug :: BinCxt a -> Bin a -> Bin a
-plug Hole t = t
-plug (B0 c x) t = Node x t (plug c t)
-plug (B1 x c) t = Node x (plug c t) t-}
-
-{-go_left :: BinZip a -> Maybe (BinZip a)
-go_left (c, Node x t1 t2) = Just (B0 c x, t1)  -- Focus on the left child
-go_left (c, Leaf _) = Nothing  -- Leaf has no left child
-
-go_right :: BinZip a -> Maybe (BinZip a)
-go_right (c, Node x t1 t2) = Just (B1 x c, t2)  -- Focus on the right child
-go_right (c, Leaf _) = Nothing  -- Leaf has no right child
-
-go_down :: BinZip a -> Maybe (BinZip a)
-go_down (B0 c x, t) = Just (c, Node x t (Leaf Nothing))  -- Focus on parent from the left child
-go_down (B1 x c, t) = Just (c, Node x (Leaf Nothing) t)  -- Focus on parent from the right child
-go_down (Hole, _) = Nothing  -- Root has no parent-}
 
 
 check_action :: Item -> Bin Item -> Bool
@@ -93,6 +76,55 @@ do_feed node = do
         putStrLn "Nothing to feed"
         return Nothing
 
+go_back :: BinZip Item -> BinZip Item
+go_back = undefined
+
+
+
+-- generate a random Item (Rock, Baby, Spider, or Nothing) 
+-- 1/2 proba of Nothing, 1/6 proba for Rock, 1/6 proba for Baby, 1/6 for Spider
+randomItem :: IO (Maybe Item)
+randomItem = do
+    randomNumber <- randomIO :: IO Int
+    if randomNumber `mod` 2 == 0 -- first choose between Nothing or Just an item
+        then return Nothing
+        else do -- then choose among the 3 possible items
+            let n = randomNumber `mod` 3
+            case n of
+                0 -> return (Just Rock)
+                1 -> return (Just Baby)
+                _ -> return (Just Spider)
+
+
+-- data structure to help us build a random binary tree 
+data SubtreeOption = LeftSubtree | RightSubtree | BothSubtrees
+
+-- generated a random subtree option with equal proba each
+randomSubtreeOption :: IO SubtreeOption
+randomSubtreeOption = do
+  randomNumber <- randomIO :: IO Int
+  case randomNumber `mod` 3 of
+    0 -> return LeftSubtree
+    1 -> return RightSubtree
+    _ -> return BothSubtrees
+
+-- generate a random binary tree with depth n
+generateTree :: Int -> IO (Bin Item)
+generateTree 0 = do --depth 0 = leaf
+  item <- randomItem
+  return (Leaf item)
+generateTree depth = do
+  item <- randomItem -- pick an item
+  subtreeOption <- randomSubtreeOption
+  leftSubtree <- case subtreeOption of --generate a left subtree if
+    LeftSubtree -> generateTree (depth - 1) -- left subtree
+    BothSubtrees -> generateTree (depth - 1) -- both
+    _ -> return (Leaf item) --otherwise, stop at a leaf
+  rightSubtree <- case subtreeOption of --same for the right substree
+    RightSubtree -> generateTree (depth - 1)
+    BothSubtrees -> generateTree (depth - 1)
+    _ -> return (Leaf item)
+  return (Node item leftSubtree rightSubtree)
 
 
 
@@ -100,7 +132,56 @@ do_feed node = do
 
 
 
+-- get the number of nodes with Nothing
+countNothingNodes :: Bin Item -> Int
+countNothingNodes (Leaf Nothing) = 1
+countNothingNodes (Leaf _) = 0
+countNothingNodes (Node Nothing left right) = 1 + countNothingNodes left + countNothingNodes right
+countNothingNodes (Node _ left right) = countNothingNodes left + countNothingNodes right
+
+
+{-
+Populates the empty nodes/leafs with either a spider or a rock
+ARG1 : tree
+ARG2 : number of spots to be populated (excpeted value)
+-}
+populateEmptyNodes :: Bin Item -> Int -> IO (Bin Item)
+populateEmptyNodes tree p = do
+    let n = countNothingNodes tree
+    go tree n
+  where
+    go (Leaf Nothing) n = do --empty Leaf
+      randomNumber1 <- randomRIO (0.0, 1.0 :: Float)
+      if randomNumber1 <= fromIntegral p / fromIntegral n --decide if we populate 
+        then do
+            randomNumber2 <- randomRIO (0, 1 :: Int) --decide with what do we populate (Rock or Spider)
+            let newItem = if (randomNumber2 == 0)
+                            then Just Rock
+                            else Just Spider
+            return (Leaf newItem)
+        else return (Leaf Nothing)
+
+    go (Node Nothing left right) n = do --empty node
+      newLeft <- go left n --try to populate the right child
+      newRight <- go right n --try to populate the left child
+      randomNumber1 <- randomRIO (0.0, 1.0 :: Float)
+      if randomNumber1 <= fromIntegral p / fromIntegral n
+        then do
+            randomNumber2 <- randomRIO (0, 1 :: Int)
+            let newItem = if (randomNumber2 == 0)
+                            then Just Rock
+                            else Just Spider
+            return (Node newItem newLeft newRight)
+      else return (Node Nothing newLeft newRight)
+
+    --when the node/leaf is already populated
+    go (Leaf item) _ = return (Leaf item)
+    go (Node item left right) n = do
+      newLeft <- go left n
+      newRight <- go right n
+      return (Node item newLeft newRight)
 
 
 
-
+--example to test that it populated well
+--let t = Node Nothing (Leaf Nothing) (Node Nothing (Leaf Nothing) (Leaf Nothing))
---
+++ Printing.hs
@@ -0,0 +1,386 @@
+import Bin
+
+import System.Console.ANSI
+
+
+
+{-
+Return the trimmed version of the input tree
+ARG1 : tree
+ARG2 : depth at which to trim
+-}
+trimTree :: Bin a -> Int -> Bin a
+trimTree (Leaf x) _ = Leaf x 
+trimTree (Node x left right) depth
+    | depth == 0 = Leaf x
+    | otherwise   = Node x (trimTree left (depth - 1)) (trimTree right (depth - 1))
+
+
+
+
+
+
+--Gets the max depth of a tree
+maxDepth :: Bin Item -> Int
+maxDepth (Leaf _) = 1
+maxDepth (Node _ left right) = 1 + max (maxDepth left) (maxDepth right)
+
+-- Fills in the given tree to get a depth n 
+fill :: Bin Item -> Int -> Bin Item
+fill tree 0 = tree
+fill (Leaf item) n = (Node item (fill (Leaf (Just NonExistant)) (n-1)) (fill (Leaf (Just NonExistant)) (n-1)))
+fill (Node item left right) n = (Node item (balanceTree left) (balanceTree right))
+
+-- Takes in a non-balanced tree and returns a balanced version with non-existant nodes
+balanceTree :: Bin Item -> Bin Item
+balanceTree (Leaf item) = (Leaf item)
+balanceTree (Node item left right) = (Node item (fill left n) (fill right m)) 
+  where
+    n = (maxDepth (Node item left right)) - (maxDepth left) - 1
+    m = (maxDepth (Node item left right)) - (maxDepth right) - 1
+
+
+
+-- Checks if a binary tree is balanced
+isBalanced :: Bin Item -> Bool
+isBalanced (Leaf _) = True 
+isBalanced (Node _ left right) =
+  let leftHeight = maxDepth left
+      rightHeight = maxDepth right
+  in
+    abs (leftHeight - rightHeight) <= 1 && isBalanced left && isBalanced right
+
+
+
+-- From a binary tree returns a list if lists where each inner list has the nodes present at a given level
+levelLabels :: Bin a -> [[Maybe a]]
+levelLabels tree = levelLabelsHelper [tree]
+  where
+    levelLabelsHelper :: [Bin a] -> [[Maybe a]]
+    levelLabelsHelper [] = []
+    levelLabelsHelper nodes =
+      let labels = concatMap getNodeLabel nodes
+          nextLevelNodes = concatMap getChildren nodes
+      in labels : levelLabelsHelper nextLevelNodes
+
+    getNodeLabel (Leaf label) = [label]
+    getNodeLabel (Node label _ _) = [label]
+
+    getChildren (Node _ left right) = [left, right]
+    getChildren _ = []
+
+
+
+
+
+--Prints items
+printItem :: Maybe Item -> String
+printItem (Just Rock) = "Rock"
+printItem (Just Spider) = "Spider"
+printItem (Just Baby) = "Baby"
+printItem (Just NonExistant) = " "
+printItem Nothing = "Empty"
+
+
+{-
+usage : ARG1 : list of the nodes at each level [[root], [children], [grandchildren], [great grandchildren]]
+        len root = 1, len children = 2, len grandchildren = 4, len great grandchildren = 8
+
+        ARG2 : list of the nodes of the sibling of the node we are at [[root], [children]]
+        len root = 1, len children = 2
+
+        ARG3 : item found at the parent of the current node
+
+        ARG4 : 0 = we took the left path (and have a right sibling)
+               1 = we took the right path (and have a left sibling)
+               2 = we are at the root and have no sibling
+-}
+prettyPrintHelper :: [[Maybe Item]] -> [[Maybe Item]] -> Maybe Item -> Int -> IO ()
+prettyPrintHelper [l1, l2, l3, l4] [c1, c2] parent_item dir = do
+
+    let expectedLengths_l = [1, 2, 4, 8]
+    let actualLengths_l = map length [l1, l2, l3, l4]
+
+    let expectedLengths_c = [1, 2]
+    let actualLengths_c = map length [c1, c2]
+
+    if expectedLengths_l /= actualLengths_l && expectedLengths_c /= actualLengths_c
+    then putStrLn "Error: The provided list isn't a valid tree"
+    else do 
+
+        -- FURTHER CHILDREN
+        putStr $ replicate 6 ' '
+        putStr " "
+        putStr $ concatMap (\i -> if l4 !! i == (Just NonExistant) then " " else "\\/" ++ replicate 12 ' ') [0, 2, 4, 6]
+        putStrLn ""
+
+        ------- GRANDCHILDREN 
+
+        -- 4      5        6       7
+        putStr $ replicate 6 ' '
+        putStr $ unwords (map (\x -> printItem x ++ replicate 8 ' ') (take 4 l3)) ++ "\n"
+
+        -- 4      5        6       7
+        -- |      |        |       |
+        putStr $ replicate 6 ' '
+        putStr "  "
+        putStr $ unwords [if x == (Just NonExistant) then " " else "|" ++ replicate 12 ' ' | x <- l3] ++ "\n"
+        
+        -- 4      5        6       7
+        -- |      |        |       |
+        -- --------        ---------
+        putStr $ replicate 6 ' '
+        putStr $ if l3 !!  0 == (Just NonExistant) then "     " else "  -------" --first half of first line
+        putStr $ if l3 !!  1 == (Just NonExistant) then "     " else "--------" ++ replicate 13 ' ' --second half of first line
+        putStr $ if l3 !!  2 == (Just NonExistant) then "     " else "-------" --first half of second line
+        putStr $ if l3 !!  3 == (Just NonExistant) then "     " else "--------" --second half of second line
+        putStrLn ""
+
+        -- 4      5        6       7
+        -- |      |        |       |
+        -- --------        ---------
+        --     |               |
+        putStr $ replicate 6 ' '
+        putStr $ replicate 9 ' '
+        if l3 !! 0 ==  (Just NonExistant) then putStr " " else putStr "|"
+        putStr $ replicate 25 ' '
+        if l3 !! 2 ==  (Just NonExistant) then putStr " " else putStr "|"
+        putStrLn ""
+
+
+        ---------- CHILDREN -------------------
+
+        -- 4      5        6       7
+        -- |      |        |       |
+        -- --------        ---------
+        --     |               |
+        --     2               3
+        putStr $ replicate 6 ' '
+        putStr $ replicate 6 ' ' ++ printItem (l2 !! 0) ++ replicate 23 ' ' --space first item space
+        putStr $ printItem (l2 !! 1)  --second item
+        putStrLn ""
+
+        -- 4      5        6       7
+        -- |      |        |       |
+        -- --------        ---------
+        --     |               |
+        --     2               3
+        --     |               |
+        putStr $ replicate 6 ' '
+        putStr $ replicate 9 ' '
+        putStr $ if l2 !!  0 == (Just NonExistant) then " " else "|"
+        putStr $ replicate 25 ' '
+        putStr $ if l2 !! 1 == (Just NonExistant) then " " else "|" 
+        putStrLn ""
+
+        -- 4      5        6       7
+        -- |      |        |       |
+        -- --------        ---------
+        --     |               |
+        --     2               3
+        --     |               |
+        --     -----------------
+        putStr $ replicate 6 ' '
+        putStr $ replicate 9 ' '
+        putStr $ if l2 !!  0 == (Just NonExistant) then "               " else "--------------" --first half
+        putStr $ if l2 !!  1 == (Just NonExistant) then "               " else "-------------"  --second half
+        putStrLn ""
+        
+        if dir == 0 then do
+            -- 4      5        6       7
+            -- |      |        |       |
+            -- --------        ---------
+            --     |               |
+            --     2               3
+            --     -----------------
+            --             |                     \/
+            putStr $ replicate 6 ' '
+            putStr $ replicate 23 ' ' ++ ['|'] 
+            putStr $ replicate 25 ' '
+            putStr $ if c2 !!  1 == (Just NonExistant) then " " else "\\/"
+            putStrLn ""
+
+            --------- ROOT 
+
+            -- 4      5        6       7
+            -- |      |        |       |
+            -- --------        ---------
+            --     |               |
+            --     2               3
+            --     -----------------
+            --             |                  \/
+            --             1                   9
+            putStr $ replicate 6 ' '
+            putStr $ replicate 22 ' ' ++printItem (l1 !! 0) 
+            putStr $ replicate 22 ' ' ++ printItem (c1 !! 0) 
+            putStrLn ""
+
+
+            -- 4      5        6       7
+            -- |      |        |       |
+            -- --------        ---------
+            --     |               |
+            --     2               3
+            --     -----------------
+            --             |                  \/
+            --             1                   9
+            --             |                   |
+            --             ---------------------
+            putStr $ replicate 6 ' '
+            putStr $ replicate 19 ' ' 
+            setSGR [SetColor Foreground Vivid Red]
+            putStr "YOU " 
+            setSGR [Reset] 
+            putStr ("|" ++ replicate 26 ' ' ++ "|")
+            putStrLn ""
+            putStr $ replicate 6 ' '
+            putStr $ replicate 23 ' ' ++ "----------------------------"
+            putStrLn ""
+
+
+            -- 4      5        6       7
+            -- |      |        |       |
+            -- --------        ---------
+            --     |               |
+            --     2               3
+            --     -----------------
+            --             |                  \/
+            --             1                   9
+            --             ---------------------
+            --                        |
+            --                      Empty
+            putStr $ replicate 6 ' '
+            putStr $ replicate 36 ' ' ++ ['|']
+            putStrLn ""
+            putStr $ replicate 34 ' ' ++ "Empty"
+            putStrLn ""
+
+        else if dir == 1 then do
+            --      4      5        6       7
+            --      |      |        |       |
+            --      --------        ---------
+            --          |               |
+            --          2               3
+            --          -----------------
+            -- \/               |                     
+            putStr $ replicate 3 ' '
+            putStr $ if c2 !!  1 == (Just NonExistant) then " " else "\\/"
+            putStr $ replicate 23 ' ' ++ ['|'] 
+            putStrLn ""
+
+            --------- ROOT 
+
+            --      4      5        6       7
+            --      |      |        |       |
+            --      --------        ---------
+            --          |               |
+            --          2               3
+            --          -----------------
+            -- \/               |    
+            --  9               1               
+            putStr $ replicate 2 ' '
+            putStr $ printItem (c1 !! 0) 
+            putStr $ replicate 20 ' ' ++printItem (l1 !! 0) 
+            putStrLn ""
+
+
+            
+            --      4      5        6       7
+            --      |      |        |       |
+            --      --------        ---------
+            --          |               |
+            --          2               3
+            --          -----------------
+            -- \/               |    
+            --  9               1     
+            --  |               |
+            --  -----------------     
+            putStr $ replicate 4 ' '
+            putStr "|"
+            putStr $ replicate 19 ' ' 
+            setSGR [SetColor Foreground Vivid Red]
+            putStr "YOU " 
+            setSGR [Reset] 
+            putStr ("|" ++ replicate 26 ' ')
+            putStrLn ""
+            putStr $ replicate 4 ' ' ++ "-------------------------"
+            putStrLn ""
+
+
+            -- 4      5        6       7
+            -- |      |        |       |
+            -- --------        ---------
+            --     |               |
+            --     2               3
+            --     -----------------
+            --             |                  \/
+            --             1                   9
+            --             ---------------------
+            --                        |
+            --                      Empty
+            putStr $ replicate 6 ' '
+            putStr $ replicate 10 ' ' ++ "|"
+            putStrLn ""
+            putStr $ replicate 15 ' '
+            putStr $ printItem parent_item
+            putStrLn ""
+
+        else do
+            --      4      5        6       7
+            --      |      |        |       |
+            --      --------        ---------
+            --          |               |
+            --          2               3
+            --          -----------------
+            --                  |                     
+            putStr $ replicate 6 ' '
+            putStr $ replicate 23 ' ' ++ ['|'] 
+            putStrLn ""
+
+            --------- ROOT 
+
+            --      4      5        6       7
+            --      |      |        |       |
+            --      --------        ---------
+            --          |               |
+            --          2               3
+            --          -----------------
+            --                  |    
+            --                  1               
+            putStr $ replicate 6 ' '
+            putStr $ replicate 22 ' ' ++ printItem (l1 !! 0) 
+            putStrLn ""
+            putStr $ replicate 29 ' '
+            putStr "|"
+            putStrLn ""
+            setSGR [SetColor Foreground Vivid Red]
+            putStr $ replicate 28 ' '
+            putStr "YOU"
+            setSGR [Reset]
+            putStrLn "" 
+
+            
+
+
+
+prettyPrint :: Bin Item -> IO() --might change the type signature to BinZip
+prettyPrint = undefined
+{-
+step 1 : trim the tree to depth 4 (we need to get root, children, grandchildren, greatgrandchildren ONLY)
+step 2 : balance the tree
+step 3 : get the list of the tree = l 
+step 4 : get the list with the sibling and the children of the sibling = c
+step 5 : get the item at the parant of the current tree = it
+step 6 : fuigure out if the sibling goes left or right = dir
+step 6 : call prettyPrintHelper l c it dir
+-}
+
+
+-- TEST
+main :: IO ()
+main = do
+    let lists = [ [(Just Rock)], [(Just Spider), (Just Baby)], [Nothing, Nothing, (Just Rock) , (Just Rock)], [(Just Rock), (Just NonExistant), (Just NonExistant), (Just NonExistant), (Just NonExistant), (Just NonExistant), (Just NonExistant), (Just NonExistant)]]
+    
+    let cs = [[(Just Baby)], [Nothing, Nothing]]
+
+    prettyPrintHelper lists cs (Just Rock) 2
